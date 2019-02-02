5d1cbe10f78340ebb6f4621de4e15a463ddc6e44
Matthew Shaw <mshaw@sparta.com>
2019-02-02 18:43:59 +0000

    shuffle funcution added

--- js/index.js
+++ js/index.js
@@ -8,8 +8,21 @@ var data = [ "UK", "Japan", "Spain", "US"];
 
 
     });
-    for (var i = 0; i < grid.length; i++) {
-      grid[i].innerHTML = data[i];
-    }
 
+
+    function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+shuffle(grid);
+
+for (var i = 0; i < grid.length; i++) {
+  grid[i].innerHTML = data[i];
+}
 });
