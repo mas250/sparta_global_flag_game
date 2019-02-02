223a0f5bb9ab4849565368e836cb0b7dc3c0b8fd
Matthew Shaw <mshaw@sparta.com>
2019-02-02 18:40:46 +0000

    basic anwser template working: anwser grid to screen and return content

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-console.log("linked");
+$(document).ready(function() {
+  console.log("linked");
+
+var data = [ "UK", "Japan", "Spain", "US"];
+
+  var grid =  $(".answer_grid").on("click", function(event){
+      console.log(this.innerHTML);
+
+
+    });
+    for (var i = 0; i < grid.length; i++) {
+      grid[i].innerHTML = data[i];
+    }
+
+});
