2fb60b03d05d5a1a7a62ca27a990fbd35d2810d9
Matthew Shaw <mshaw@sparta.com>
2019-02-04 10:24:08 +0000

    anwser checking started to be added

--- js/index.js
+++ js/index.js
@@ -1,28 +1,52 @@
 $(document).ready(function() {
   console.log("linked");
 
-var data = [ "UK", "Japan", "Spain", "US"];
-
+  var data = [ "UK", "Japan", "Spain", "US"];
+var answer = "";
   var grid =  $(".answer_grid").on("click", function(event){
-      console.log(this.innerHTML);
+
+    console.log(this.innerHTML);
+    answer = this.innerHTML;
+    console.log("anwser:", answer);
+    if (answer == "UK"){
+      alert("progress");
+    }
+    return answer;
+  });
+
+  var mast = $(".mast");
+  var allFlags = ['<img src="images/UK.jpg" >','<img src="images/spain.jpg" >','<img src="images/japan.jpg" >', '<img src="images/us.jpg" >'];
+
+  var allFlags2 = [ ['<img src="images/UK.jpg" >', "UK"],
+                    ['<img src="images/spain.jpg" >',"Spain" ],
+                    ['<img src="images/japan.jpg" >' ,"Japan"],
+                    ['<img src="images/us.jpg" >', "US"] ]
 
 
-    });
+  function getFlag(allFlags2){
+    var item = allFlags2[Math.floor(Math.random()*allFlags.length)];
+    // var newFlag = item //'<img src="images/UK.jpg" >';
+    // return newFlag;
+    return item;
+  }
 
-
-    function shuffle(a) {
+  function shuffle(a) {
     var j, x, i;
     for (i = a.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = a[i];
-        a[i] = a[j];
-        a[j] = x;
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
     }
     return a;
-}
-shuffle(grid);
+  }
+  shuffle(grid);
 
-for (var i = 0; i < grid.length; i++) {
-  grid[i].innerHTML = data[i];
-}
+  for (var i = 0; i < grid.length; i++) {
+    grid[i].innerHTML = data[i];
+  }
+
+  mast[0].innerHTML = getFlag(allFlags);
+
+
 });
