46899a343e24d7dacb9722df3a84bff7d25dd8d9
Matthew Shaw <mshaw@sparta.com>
2019-02-07 20:29:06 +0000

    extreme mode workaround

--- js/extreme.js
+++ js/extreme.js
@@ -3,19 +3,24 @@
   var xPosition = event.clientX;  //capture coordinates from the map
   var yPosition = event.clientY;
 
-  console.log(xPosition + " " + yPosition);
+  // console.log(xPosition + " " + yPosition);
   //check bounds of country and display corresponding map
   if (((xPosition > 84) && (xPosition < 121)) && ((yPosition > 59) && (yPosition < 159))  ) {
-    mast[0].innerHTML = '<img src="images/UK.jpg">'
+    mast[0].innerHTML = '<img src="images/UK.jpg">';
+    $(".extremeName").text("UK");
+
   }
   if (((xPosition > 34) && (xPosition < 64)) && ((yPosition > 94) && (yPosition < 131))  ) {
-    mast[0].innerHTML = '<img src="images/ireland.jpg">'
+    mast[0].innerHTML = '<img src="images/ireland.jpg">';
+    $("#ireland").wrap("<del>")
   }
   if (((xPosition > 12) && (xPosition < 76)) && ((yPosition > 281) && (yPosition < 357))  ) {
-    mast[0].innerHTML = '<img src="images/spain.jpg">'
+    mast[0].innerHTML = '<img src="images/spain.jpg">';
+    $(".extremeName").text("Spain");
   }
   if (((xPosition > 63) && (xPosition < 169)) && ((yPosition > 178) && (yPosition < 297))  ) {
-    mast[0].innerHTML = '<img src="images/france.jpg">'
+    mast[0].innerHTML = '<img src="images/france.jpg">';
+    $("#france").wrap("<del>")
   }
 
 });
